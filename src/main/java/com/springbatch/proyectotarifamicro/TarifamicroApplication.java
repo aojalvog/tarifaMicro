c21e613e7a47b40edd26d24054bf64fc8ce87ebe
aojalvog <alvaro.ojalvo@gmail.com>
2024-05-09 17:47:01 +0200

    feat(Creación del proyecto): :sparkles:

---
+++ src/main/java/com/springbatch/proyectotarifamicro/TarifamicroApplication.java
@@ -0,0 +1,13 @@
+package com.springbatch.proyectotarifamicro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TarifamicroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TarifamicroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springbatch/proyectotarifamicro/TarifamicroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbatch.proyectotarifamicro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TarifamicroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
